e323edca6b6be52af47d213f9391ff9e05ec7976
geanlucasgit <gitgeanlucas@gmail.com>
2023-06-12 22:42:51 -0300

    atualização READ.ME

--- bancoSQL.sql
+++ bancoSQL.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `books`
 --
---limpesa de cache para a criaçao da tabela
+--limpeza de cache para a criaçao da tabela
 DROP TABLE IF EXISTS `books`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
